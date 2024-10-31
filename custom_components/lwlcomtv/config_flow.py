b890fa714fc3f2dac370cbe8be85617b04926ea4
timniklas <accounts@tnkoch.de>
2024-10-31 02:10:07 +0100

    Create config_flow.py

---
+++ custom_components/lwlcomtv/config_flow.py
@@ -0,0 +1,11 @@
+from homeassistant.config_entries import ConfigFlow
+from .const import DOMAIN
+
+class LwlcomConfigFlow(ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
+    def __init__(self) -> None:
+        """Initialize the config flow."""
+
+    async def async_step_user(self, formdata):
+        return self.async_create_entry(title="LWLcom TV")
